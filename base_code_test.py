831ada4c82a7aaabe03b5b062eb82b0ab1d53f38
qindongliang <qindongliang@xueleyun.com>
2019-01-19 11:00:08 +0800

    增加测试用例

--- base_code_test.py
+++ base_code_test.py
@@ -22,13 +22,14 @@ def base_code():
     log.addHandler(handler_console)
 
     try:
-        log.info("message1")
+        log.info("message1","ms2","ms3",[1,2,3])
+        log.info([1,23,45])
         # log.info("message2", {"extra_info": " trace info"})
         # log.info({"message3": " log info"})
         a = 1 / 0
     except Exception:
         # log.error("occurred exception", exc_info=True)
-        log.exception({"exception_id": 22000})
+        log.exception("报错了")
         # log.exception("occurred exception")
         # log.exception("occurred exception", {"exception_id":225462})
 
